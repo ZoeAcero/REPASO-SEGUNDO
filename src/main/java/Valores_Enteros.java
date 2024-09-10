0d9b50d708b0fd6466e43e8c79f05b193d30a570
ZoeAcero <zar.amazona@gmail.com>
2024-09-10 16:11:31 +0200

    a

--- src/main/java/Valores_Enteros.java
+++ src/main/java/Valores_Enteros.java
@@ -4,5 +4,9 @@ public class Valores_Enteros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Introduce el primer valor entero:");
+        int valor1 = scanner.nextInt();
+
+        System.out.println("Introduce el segundo valor entero:");
+        int valor2 = scanner.nextInt();
 }

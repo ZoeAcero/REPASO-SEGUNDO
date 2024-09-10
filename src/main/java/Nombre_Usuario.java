c4baa0288f7cdb659e465250653e2deda62422a9
ZoeAcero <zar.amazona@gmail.com>
2024-09-10 16:17:19 +0200

    a

--- src/main/java/Nombre_Usuario.java
+++ src/main/java/Nombre_Usuario.java
@@ -1,4 +1,12 @@
 import java.util.Scanner;
 
 public class Nombre_Usuario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce tu nombre:");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Bienvenido al curso de M03B, " + nombre);
+    }
 }

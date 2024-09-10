1ecafadeb566987ea285b6c7f89e2cbc0568d61d
ZoeAcero <zar.amazona@gmail.com>
2024-09-10 16:11:56 +0200

    a

--- src/main/java/Valores_Enteros.java
+++ src/main/java/Valores_Enteros.java
@@ -9,4 +9,15 @@ public class Valores_Enteros {
 
         System.out.println("Introduce el segundo valor entero:");
         int valor2 = scanner.nextInt();
+
+        int suma = valor1 + valor2;
+        int resta = valor1 - valor2;
+        int producto = valor1 * valor2;
+        double division = (double) valor1 / valor2;
+
+        System.out.println("La suma de los valores es: " + suma);
+        System.out.println("La resta de los valores es: " + resta);
+        System.out.println("El producto de los valores es: " + producto);
+        System.out.println("La división de los valores es: " + division);
+    }
 }
